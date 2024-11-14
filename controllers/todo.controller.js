79f48db67de03e58feb7e411ebf76101f74bafe0
martpors <M2rtpors@hotmail.com>
2024-11-14 14:24:27 +0200

    .

---
+++ controllers/todo.controller.js
@@ -0,0 +1,51 @@
+import { PrismaClient, TodoStatus } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export const getAllTodos = async (request, response) => {
+
+    try {
+
+        const todos = await prisma.todo.findMany()
+
+        response.status(200).json({
+            todos
+        })
+        
+    } catch (error) {
+            console.log(error)
+            response.status(500).json({
+                message: "Bad luck. Try again."
+        })
+    }
+}
+
+export const createTodo = async (request, response) => {
+    // const title = request.body.title
+    // const description = request.body.description
+
+    const { title, description } = request.body
+
+    try {
+
+        const todo = await prisma.todo.create({
+            data: {
+                title,
+                description,
+                status: TodoStatus.TODO
+            }
+        })
+
+        response.status(201).json({
+            message: "Todo created successfully.",
+            todo
+        })
+
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({
+            message: "Bad luck. Try again."
+        })
+    }
+
+}
